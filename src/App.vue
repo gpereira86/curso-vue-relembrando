215a5445a75b16bbb49694d795a96c329d47f685
Glauco Pereira <glaucoadm@gmail.com>
2025-05-06 15:53:01 -0300

    Classe e clique

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@
   // import Exemplo02 from './componentes/Exemplo02Condicional.vue'
   // import Exemplo03 from './componentes/Exemplo03Laco.vue'
   // import Exemplo04 from './componentes/Exemplo04HTML.vue'
-  import Exemplo05 from './componentes/Exemplo05Model.vue'
+  // import Exemplo05 from './componentes/Exemplo05Model.vue'
+  // import Exemplo06 from './componentes/Exemplo06Classe.vue'
+  import Exemplo07 from './componentes/Exemplo07Clique.vue'
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo05></Exemplo05>
+  <Exemplo07></Exemplo07>
 </template>
---
+++ src/componentes/Exemplo06Classe.vue
@@ -0,0 +1,16 @@
+<!-- SCRIPT -->
+<script setup>
+  // Variável
+  let validacao = false;
+</script>
+
+<!-- CSS -->
+<style>
+    .ok{color: green;}
+    .falha{color: red;}
+</style>
+
+<!-- HTML -->
+<template>
+    <p :class="validacao ? 'ok' : 'falha'">Utilizando a Diretiva class</p>
+</template>
