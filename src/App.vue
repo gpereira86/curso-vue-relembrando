9172993b7e1d9605a2086609c4b523b808a009e0
Glauco Pereira <glaucoadm@gmail.com>
2025-05-06 13:45:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<!-- Script -->
+<script setup>
+  // Variáveis
+  let nome = 'Alice'
+  let idade = 25
+  let numero1 = 10;
+  let numero2 = 5;
+
+</script>
+
+<!-- HTML -->
+<template>
+  <p>{{ 'Olá ' + nome + ' você tem ' + idade + ' anos.'}}</p>
+  <p>{{ numero1 + numero2 }}</p>
+</template>
