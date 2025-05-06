afae080e96273b488f67c87fbbc4b7a42ed4a6b1
Glauco Pereira <glaucoadm@gmail.com>
2025-05-06 19:24:08 -0300

    Rotas

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <!-- SCRIPT -->
 <script setup>
   // Importação de componentes
-  // import Exemplo01 from './componentes/Exemplo01interpolacao.vue'
-  // import Exemplo02 from './componentes/Exemplo02Condicional.vue'
-  // import Exemplo03 from './componentes/Exemplo03Laco.vue'
-  // import Exemplo04 from './componentes/Exemplo04HTML.vue'
-  // import Exemplo05 from './componentes/Exemplo05Model.vue'
-  // import Exemplo06 from './componentes/Exemplo06Classe.vue'
-  // import Exemplo08 from './componentes/Exemplo08Teclado.vue'
-  // import Exemplo09 from './componentes/Exemplo09Enviar.vue'
-  // import Exemplo10 from './componentes/Exemplo10Lifecycle.vue'
-  import Exemplo11 from './componentes/Exemplo11Props1.vue'
+  // import Exemplo01 from './componentes/Exemplo01interpolacao.vue';
+  // import Exemplo02 from './componentes/Exemplo02Condicional.vue';
+  // import Exemplo03 from './componentes/Exemplo03Laco.vue';
+  // import Exemplo04 from './componentes/Exemplo04HTML.vue';
+  // import Exemplo05 from './componentes/Exemplo05Model.vue';
+  // import Exemplo06 from './componentes/Exemplo06Classe.vue';
+  // import Exemplo08 from './componentes/Exemplo08Teclado.vue';
+  // import Exemplo09 from './componentes/Exemplo09Enviar.vue';
+  // import Exemplo10 from './componentes/Exemplo10Lifecycle.vue';
+  // import Exemplo11 from './componentes/Exemplo11Props1.vue';
+  // import Exemplo12 from './componentes/Exemplo12Rotas.vue';
+  import Exemplo13 from './componentes/Exemplo13Requisicao.vue';
+
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo11></Exemplo11>
+  <Exemplo13></Exemplo13>
 </template>
---
+++ src/componentes/Exemplo13Requisicao.vue
@@ -0,0 +1,10 @@
+<!-- SCRIPT -->
+<script setup>
+  // Variável
+      
+</script>
+
+<!-- HTML -->
+<template>
+    <h1>Requisições</h1>
+</template>
