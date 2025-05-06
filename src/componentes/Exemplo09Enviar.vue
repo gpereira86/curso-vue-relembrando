b9e4a138d266c42ab464d8795f70d3246c4e2230
Glauco Pereira <glaucoadm@gmail.com>
2025-05-06 16:20:07 -0300

    @submit (enviar form) e event.preventDefault

--- src/componentes/Exemplo09Enviar.vue
+++ src/componentes/Exemplo09Enviar.vue
@@ -1,13 +1,19 @@
 <!-- SCRIPT -->
 <script setup>
   // Função para validar o formulário
-  function validaFormulario() {
-    
-  }
+  function validaFormulario(event) {
+    alert('Formulário enviado');
 
+    event.preventDefault();
+  }
 </script>
 
 <!-- HTML -->
 <template>
-    
+    <form action="https://glaucopereira.com" @submit="validaFormulario">
+        <input type="submit" value="Enviar formulário"> &nbsp;
+        <button type="submit">Teste Buton</button>
+    </form>
+    <p>Por qual motivo ele usou um input como button? Pesquisar</p>
+    <p>Dica JS: EventpreventDefault -> Evita o acionamento do action no form </p>
 </template>
