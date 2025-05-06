c3ac5eb18e687accb4cab87ad448c94aeeeee6b8
Glauco Pereira <glaucoadm@gmail.com>
2025-05-06 19:32:14 -0300

    Requisição GET

--- src/componentes/Exemplo13Requisicao.vue
+++ src/componentes/Exemplo13Requisicao.vue
@@ -1,10 +1,30 @@
 <!-- SCRIPT -->
 <script setup>
-  // Variável
+
+  // Importações
+  import{ onMounted, ref } from 'vue';
+  
+  // vetor contendo as postagens
+  let vetor = ref([]);
+
+  onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(retorno => retorno.json())
+        .then(retorno => vetor.value = retorno)
+  });
       
 </script>
 
 <!-- HTML -->
 <template>
     <h1>Requisições</h1>
+
+    <table>
+      <tr v-for="v in vetor">
+        <td>{{ v.id }}</td>
+        <td>{{ v.userId }}</td>
+        <td>{{ v.title }}</td>
+        <td>{{ v.body }}</td>
+      </tr>
+    </table>
 </template>
